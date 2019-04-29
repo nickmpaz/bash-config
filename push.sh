86b970bac620ba33b20858968b161514f9d5846f
Nick Paz <nicholasmpaz@gmail.com>
2019-04-29 11:52:18 -0700

    typo

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ cd bash-config
 echo "copying"
 cp ./.bashrc ~/.bashrc
 cp ./.bash_aliases ~/.bash_aliases
-cp ./vimrc ~/.vimrc
+cp ./.vimrc ~/.vimrc
 cp -r ./.functions/ ~/.functions/
 echo "sourcing"
 . ~/.bashrc && . ~/.vimrc && . ~/.bash_aliases
