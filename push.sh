69f671f9c39a6180097b0838e9488c62e3f0f36f
Nick Paz <nicholasmpaz@gmail.com>
2019-04-29 11:51:29 -0700

    test

--- push.sh
+++ push.sh
@@ -1,8 +1,9 @@
 cd bash-config
 
+echo "copying"
 cp ./.bashrc ~/.bashrc
 cp ./.bash_aliases ~/.bash_aliases
 cp ./vimrc ~/.vimrc
 cp -r ./.functions/ ~/.functions/
-
+echo "sourcing"
 . ~/.bashrc && . ~/.vimrc && . ~/.bash_aliases
